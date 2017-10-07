2548d3871400937ee914f7082eea2fc1b77be641
jeremiahogutu <jeremiahogutu@protonmail.com>
2017-10-07 17:59:13 -0400

    Generate link scaffold

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+require File.expand_path('../initializers/abstract_mysql2_adapter.rb', __FILE__)
 # Initialize the Rails application.
 Raddit::Application.initialize!
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171007214125) do
+
+  create_table "links", force: true do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
